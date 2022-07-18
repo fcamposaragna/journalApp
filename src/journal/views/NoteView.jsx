3192c2f9f97b5e6addf0460421eae964b1505e8c
fcamposaragna <fcamposaragna@gmail.com>
2022-07-18 18:03:04 -0300

    activate and update note in sidebar and sweetalert added

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,13 +1,19 @@
-import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
+import { useMemo, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Button, Grid, TextField, Typography } from '@mui/material'
-import { useForm } from '../../hooks/useForm';
 import { SaveOutlined } from '@mui/icons-material';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
+
+import { useForm } from '../../hooks/useForm';
 import { ImageGallery } from '../components';
+import { setActiveNote, startSaveNote } from '../../store/journal';
 
 export const NoteView = () => {
 
-    const { active: note } = useSelector(state=>state.journal);
+    const dispatch = useDispatch();
+    const { active: note, messageSaved, isSaving } = useSelector(state=>state.journal);
     const { body, title, onInputChange, formState, date } = useForm(note);
 
     const dateString = useMemo(()=>{
@@ -15,6 +21,21 @@ export const NoteView = () => {
         return newDate.toUTCString();
     }, [date])
 
+    const onSaveNote = ()=>{
+        dispatch(startSaveNote());
+    }
+
+    useEffect(() => {
+      dispatch(setActiveNote(formState));
+    }, [formState])
+
+    useEffect(() => {
+      if(messageSaved.length>0){
+        Swal.fire('Nota actualizada', messageSaved, 'success')
+      }
+    }, [messageSaved])
+    
+    
 
   return (
     <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{mb:1}}       className="animate__animated animate__fadeIn animate__faster" >
@@ -25,7 +46,12 @@ export const NoteView = () => {
         </Grid>
 
         <Grid item>
-            <Button color='primary' sx={{padding: 2}}>
+            <Button 
+                disabled={isSaving}
+                color='primary'
+                sx={{padding: 2}}
+                onClick={onSaveNote}
+            >
                 <SaveOutlined sx={{fontSize: 30, mr: 1}}/>
                 Guardar
             </Button>
